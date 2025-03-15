c6a482f6f25f0d25b4ccf23deff537050ad6b7ea
Njuguna <joshuangich2@gmail.com>
2025-03-15 04:11:42 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const lists = document.getElementById("tasks");
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description").value;
+
+    if (taskInput.trim() !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = taskInput;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "❌";
+      deleteButton.style.marginLeft = "15px";
+
+      deleteButton.addEventListener("click", () => {
+        lists.removeChild(listItem);
+      });
+
+      listItem.appendChild(deleteButton);
+      lists.appendChild(listItem);
+      form.reset();
+    }
+  });
 });
